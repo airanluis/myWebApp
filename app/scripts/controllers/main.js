358a6e9a907d63e23ca62ba6a9dcbef4856ca24d
Airan <airan.luis@gmail.com>
2015-03-15 21:04:12 +0100

    Refactor navigation sticky code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,4 +9,22 @@
  */
 angular.module('airanAppApp')
   .controller('MainCtrl', function ($scope) {
+  	$(window).bind('scroll', function() {
+    var navHeight = $("#main_overlay").height();
+    var windscroll = $(window).scrollTop();
+     if (windscroll >= navHeight) {
+        $('#sticky_nav').addClass('goToTop')
+        $('.wrapper').each(function(i) {
+            if ($(this).position().top <= windscroll+80) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+       $('#sticky_nav').removeClass('goToTop')
+       $('nav a.active').removeClass('active');
+       $('nav a:first').addClass('active');
+    }    
+  });
   });
